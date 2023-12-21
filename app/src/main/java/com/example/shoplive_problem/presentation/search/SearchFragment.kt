480b7b72240440a7b97ff7ce6550ce8b38ffca2a
iamheesoo <beforeheesoo@gmail.com>
2023-12-21 21:49:49 +0900

    [Add] 기본 화면 구성

---
+++ app/src/main/java/com/example/shoplive_problem/presentation/search/SearchFragment.kt
@@ -0,0 +1,22 @@
+package com.example.shoplive_problem.presentation.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.shoplive_problem.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    private val binding: FragmentSearchBinding by lazy {
+        FragmentSearchBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+}
