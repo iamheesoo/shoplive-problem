4773f4f484172716cc3ef5d234e58971cda23342
iamheesoo <beforeheesoo@gmail.com>
2023-12-22 21:51:05 +0900

    [Add] 검색결과 페이징 처리

--- app/src/main/java/com/example/shoplive_problem/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/example/shoplive_problem/presentation/search/SearchFragment.kt
@@ -4,17 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shoplive_problem.databinding.FragmentSearchBinding
 import com.example.shoplive_problem.presentation.extensions.showToast
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import reactivecircus.flowbinding.android.widget.editorActionEvents
+import reactivecircus.flowbinding.android.widget.textChanges
 
 class SearchFragment : Fragment() {
+
+    companion object {
+        const val SEARCH_WORD_MIN_LENGTH = 2
+    }
+
     private val binding: FragmentSearchBinding by lazy {
         FragmentSearchBinding.inflate(layoutInflater)
     }
@@ -50,15 +58,28 @@ class SearchFragment : Fragment() {
             rvList.layoutManager = GridLayoutManager(root.context, 2)
             rvList.adapter = adapter
 
-            etSearch.editorActionEvents {
-                if (it.actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    val text = etSearch.text.trim().toString()
-                    if (text.isNotEmpty()) {
-                        viewModel.getSearchResult(text)
+            rvList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+
+                    if (!recyclerView.canScrollVertically(1) && newState == RecyclerView.SCROLL_STATE_IDLE) {
+                        viewModel.getSearchResult()
                     }
                 }
-                false
-            }.launchIn(viewLifecycleOwner.lifecycleScope)
+            })
+
+            etSearch
+                .textChanges()
+                .skipInitialValue()
+                .map { it.trim() }
+                .debounce(300L)
+                .onEach { text ->
+                    viewModel.searchText = text.toString()
+                    if (text.length > SEARCH_WORD_MIN_LENGTH) {
+                        viewModel.getSearchResult()
+                    }
+                }
+                .launchIn(viewLifecycleOwner.lifecycleScope)
         }
     }
 }
--- app/src/main/java/com/example/shoplive_problem/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/example/shoplive_problem/presentation/search/SearchViewModel.kt
@@ -7,13 +7,17 @@ import androidx.lifecycle.viewModelScope
 import com.example.shoplive_problem.data.network.ResultData
 import com.example.shoplive_problem.domain.model.Character
 import com.example.shoplive_problem.domain.usecase.GetCharacterListUseCase
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 
 class SearchViewModel(
     private val getCharacterListUseCase: GetCharacterListUseCase
 ) : ViewModel() {
+
+    companion object {
+        const val LIMIT = 10
+    }
 
     private val _characterList = MutableLiveData<List<Character>>()
     val characterList: LiveData<List<Character>>
@@ -23,27 +27,50 @@ class SearchViewModel(
     val toastMessage: LiveData<String>
         get() = _toastMessage
 
-    fun getSearchResult(text: String) {
-        viewModelScope.launch {
-            getCharacterListUseCase(
-                GetCharacterListUseCase.Params(
-                    text = text,
-                    limit = 10,
-                    offset = _characterList.value?.size ?: 0
-                )
-            )
-                .collectLatest {
-                    when (it) {
-                        is ResultData.Success -> {
-                            _characterList.value = it.data
-                        }
+    var searchText: String = ""
+        set(value) {
+            field = value
+            clearData()
+        }
+    var isPagingAvailable: Boolean = true
 
-                        is ResultData.Error -> {
-                            _toastMessage.value = "서버 에러입니다.\n잠시 후 다시 시도해 주세요."
+    private var job: Job? = null
+
+    fun getSearchResult() {
+        if (isPagingAvailable) {
+            job = viewModelScope.launch {
+                getCharacterListUseCase(
+                    GetCharacterListUseCase.Params(
+                        text = searchText,
+                        limit = LIMIT,
+                        offset = _characterList.value?.size ?: 0
+                    )
+                )
+                    .collectLatest {
+                        when (it) {
+                            is ResultData.Success -> {
+                                if (it.data?.isNotEmpty() == true) {
+                                    _characterList.value = (_characterList.value ?: emptyList())
+                                        .toMutableList().apply { addAll(it.data) }
+                                } else {
+                                    isPagingAvailable = false
+                                }
+                            }
+
+                            is ResultData.Error -> {
+                                _toastMessage.value = "서버 에러입니다.\n잠시 후 다시 시도해 주세요."
+                                isPagingAvailable = false
+                            }
                         }
                     }
-                }
-
+            }
         }
+    }
+
+    private fun clearData() {
+        // 검색어가 변경되면 화면에 노출되는 리스트, 페이징 변수, 진행 중이던 api 호출을 초기화한다
+        isPagingAvailable = true
+        _characterList.value = null
+        job?.cancel()
     }
 }
